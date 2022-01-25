1af6abc6cedc89a9acee680509fa6be4c69b3714
usulemre <83465642+usulemre@users.noreply.github.com>
2022-01-25 19:45:15 +0300

    useReducer

--- src/components/Ingredient/Ingredients.js
+++ src/components/Ingredient/Ingredients.js
@@ -1,18 +1,56 @@
-import React, { Fragment, useCallback, useEffect, useState } from "react";
+import React, {
+  Fragment,
+  useCallback,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 import IngredientList from "./IngredientList";
 import Search from "./Search";
 import IngredientForm from "./IngredientForm";
 import ErorModal from "../UI/ErorModal";
 
+const fetchReducer = (currentIngredient, action) => {
+  switch (action.type) {
+    case "SET":
+      return action.ıngredients;
+    case "ADD":
+      return [...currentIngredient, action.ıngredients];
+    case "DELETE":
+      return currentIngredient.filter((prev) => prev.id !== action.id);
+    default:
+      throw new Error("Tekrar Deneyin...");
+  }
+};
+
+const httpReducer = (curHttpState, action) => {
+  switch (action.type) {
+    case "SEND":
+      return { loading: true, error: null };
+    case "RESPONSE":
+      return { ...curHttpState, loading: false };
+    case "ERROR":
+      return { loading: false, error: action.errorMessage };
+    case "CLEAR":
+      return { ...curHttpState, error: null };
+    default:
+      throw new Error("Should not be reached!");
+  }
+};
+
 const Ingredients = () => {
-  const [ıngredients, setIngredients] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [ıngredients, dispatch] = useReducer(fetchReducer, []);
+  // const [ıngredients, setIngredients] = useState([]);
+  const [httpState, dispatchHtpp] = useReducer(httpReducer, {
+    loading: false,
+    error: null,
+  });
 
   useEffect(() => {
-    setLoading(true);
+    dispatchHtpp({ type: "SEND" });
     fetch("https://todolist-af3b0-default-rtdb.firebaseio.com/ingredient.json")
       .then((response) => {
+        dispatchHtpp({ type: "RESPONSE" });
         return response.json();
       })
       .then((data) => {
@@ -24,13 +62,13 @@ const Ingredients = () => {
             amount: data[key].amount,
           });
         }
-        setLoading(false);
-        setIngredients(transform);
+        dispatch({ type: "SET", ıngredients: transform });
+        // setIngredients(transform);
       });
   }, []);
 
   const addHandler = (currentData) => {
-    setLoading(true);
+    dispatchHtpp({ type: "SEND" });
     fetch(
       "https://todolist-af3b0-default-rtdb.firebaseio.com/ingredient.json",
       {
@@ -42,42 +80,41 @@ const Ingredients = () => {
       }
     )
       .then((respense) => {
+        dispatchHtpp({ type: "RESPONSE" });
         return respense.json();
       })
       .then((data) => {
-        setLoading(false);
-        setIngredients((prevState) => [
-          ...prevState,
-          { id: data.name, ...currentData },
-        ]);
-      })
-      .catch((error) => {
-        setError(error.message || "Gönderme Hatası...");
+        dispatch({
+          type: "ADD",
+          ıngredients: { id: data.name, ...currentData },
+        });
+        // setIngredients((prevState) => [
+        //   ...prevState,
+        //   { id: data.name, ...currentData },
+        // ]);
       });
   };
 
   const filterIngredients = useCallback((currentData) => {
-    setIngredients(currentData);
+    dispatch({ type: "SET", ıngredients: currentData });
   }, []);
 
   const deleteHandler = (ıngredientId) => {
-    setIngredients((prevState) =>
-      prevState.filter((prev) => prev.id !== ıngredientId)
-    );
+    dispatch({ type: "DELETE", id: ıngredientId });
   };
 
   const onCancel = () => {
-    setError(null);
+    dispatchHtpp({ type: "CLEAR" });
   };
   return (
     <Fragment>
-      {error && <ErorModal onCancel={onCancel} />}
+      {httpState.error && <ErorModal onCancel={onCancel} />}
       <IngredientForm onAdd={addHandler} />
-      <Search onIngredient={filterIngredients} loading={loading}/>
+      <Search onIngredient={filterIngredients} loading={httpState.loading} />
       <IngredientList
         ıngredients={ıngredients}
         onDelete={deleteHandler}
-        onLoading={loading}
+        onLoading={httpState.loading}
       />
     </Fragment>
   );
